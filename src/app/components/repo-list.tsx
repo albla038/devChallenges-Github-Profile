fc2cbcd2fc74515fc68101d8d4df0be375be16aa
Albert Lännerström <albert.lannerstrom@gmail.com>
2024-08-22 14:52:42 +0200

    Added repo typing

--- src/app/components/repo-list.tsx
+++ src/app/components/repo-list.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import RepoCard from "./repo-card";
+import { reposT } from "../lib/types";
 
 type RepoSectionProps = {
   repos_url: string;
@@ -7,30 +8,36 @@ type RepoSectionProps = {
 
 export default async function RepoList({ repos_url }: RepoSectionProps) {
   const req = await fetch(repos_url);
-  const data = await req.json();
 
-  // Get the first 4 repos
-  const repos = data.slice(0, 4);
+  if (!req.ok) {
+    return <p>Error: {req.status}</p>;
+  
+  } else {
+    const data: reposT[] = await req.json();
 
-  return (
-    <section
-      className={clsx([
-        "flex flex-col gap-11",
-        "md:grid md:grid-cols-2 md:grid-rows-2",
-      ])}
-    >
-      {repos.map((repo) => (
-        <RepoCard
-          key={repo.id}
-          name={repo.name}
-          description={repo.description}
-          license={repo.license}
-          forksCount={repo.forks_count}
-          stargazersCount={repo.stargazers_count}
-          updatedAt={repo.updated_at}
-          url={repo.html_url}
-        />
-      ))}
-    </section>
-  );
+    // Get the first 4 repos
+    const repos = data.slice(0, 4);
+
+    return (
+      <section
+        className={clsx([
+          "flex flex-col gap-11",
+          "md:grid md:grid-cols-2 md:grid-rows-2",
+        ])}
+      >
+        {repos.map((repo) => (
+          <RepoCard
+            key={repo.id}
+            name={repo.name}
+            description={repo.description || ""}
+            license={repo.license}
+            forksCount={repo.forks_count}
+            stargazersCount={repo.stargazers_count}
+            updatedAt={repo.updated_at}
+            url={repo.html_url}
+          />
+        ))}
+      </section>
+    );
+  }
 }
